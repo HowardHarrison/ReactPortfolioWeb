c21247557ee1ec9bc1610ed56f89438f02e2ee95
peterpeter19 <>
2023-03-30 02:15:25 +0000

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import Profile from './components/Profile'
+import Projects from './components/Projects'
+import Certificates from './components/Certificates' 
+import Contact from './components/Contact'
 
 export default function App() {
   return (
-    <main>
-      Portfolio Website with React
+    <main className="bg-red-200">
+      <Profile/>
+      <Projects/>
+      <Certificates/>
+      <Contact/>
     </main>
   )
 }
